82e6cb21e435e7eb8e65a54f72cc1fa1d6083543
xlc-dev <119954023+xlc-dev@users.noreply.github.com>
2024-12-20 22:25:52 +0100

    upgrade nexus to 0.0.2
    Changelog:
    
    - Better build system that is less repetitive
    - 100 char clang-format
    - nx_die macro

--- nexus.h
+++ nexus.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2024 Nexus version 0.0.1
+/* Copyright (C) 2024 Nexus version 0.0.2
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
