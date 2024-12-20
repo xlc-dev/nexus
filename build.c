4e08cd962b599279c81f51e92b0784b8812c8f77
xlc-dev <119954023+xlc-dev@users.noreply.github.com>
2024-12-20 22:22:07 +0100

    use new nexus cleaner build

--- build.c
+++ build.c
@@ -1,36 +1,33 @@
 #define NEXUS_IMPLEMENTATION
 #include "nexus.h"
-#include <stdio.h>
-#include <stdlib.h>
+
+/* clang-format off */
+#define COMMON_FLAGS \
+    "-fdiagnostics-color=always", \
+    "-fPIE", \
+    "-ansi", \
+    "-O2", \
+    "-march=native", \
+    "-flto"
+/* clang-format on */
 
 int main(int argc, char **argv) {
     NX_REBUILD(argc, argv);
 
-    NXCR *cr = nx_cr_create();
-    if (!cr) {
-        fprintf(stderr, "Failed to create NXCR instance.\n");
-        return EXIT_FAILURE;
+    /* clang-format off */
+    const char *nexus_args[] = {
+        "cc",
+        "nexus.c",
+        "-o",
+        "nexus",
+        COMMON_FLAGS,
+    };
+    /* clang-format on */
+
+    int nexus_result = nx_compile_command("nexus", nexus_args, nx_len(nexus_args), 1);
+    if (nexus_result != 0) {
+        return nexus_result;
     }
 
-    nx_cr_append(cr, "cc");
-    nx_cr_append(cr, "nexus.c");
-    nx_cr_append(cr, "-o");
-    nx_cr_append(cr, "nexus");
-    nx_cr_enable_gcc_warnings(cr);
-    nx_cr_append(cr, "-fdiagnostics-color=always");
-    nx_cr_append(cr, "-fPIE");
-    nx_cr_append(cr, "-ansi");
-    nx_cr_append(cr, "-O2");
-    nx_cr_append(cr, "-march=native");
-    nx_cr_append(cr, "-flto");
-
-    int result = nx_cr_execute(cr);
-
-    if (result != 0) {
-        printf("%sCompilation failed. Output:%s\n%s\n", COLOR_RED, COLOR_RESET,
-               nx_cr_get_output(cr));
-    }
-
-    nx_cr_destroy(cr);
-    return result;
+    return EXIT_SUCCESS;
 }
