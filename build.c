ebeef5bf947a003ae60810bb0ef169f6ef9201a7
xlc-dev <119954023+xlc-dev@users.noreply.github.com>
2024-12-21 22:30:27 +0100

    add clean() and print_help() static function

--- build.c
+++ build.c
@@ -11,8 +11,38 @@
     "-flto"
 /* clang-format on */
 
+static void print_help() {
+    printf("Usage: nexus [options]\n\n");
+    printf("Options:\n");
+    printf("  --help, -h       Show this help menu\n");
+    printf("  --clean, -c      Remove generated files\n");
+}
+
+static int clean(const char *files[], size_t file_count) {
+    for (size_t i = 0; i < file_count; i++) {
+        if (remove(files[i]) != 0) {
+            perror(files[i]);
+            return EXIT_FAILURE;
+        }
+    }
+    return EXIT_SUCCESS;
+}
+
 int main(int argc, char **argv) {
     NX_REBUILD(argc, argv);
+
+    const char *files_to_remove[] = {"glad.o", "nexus"};
+
+    for (int i = 1; i < argc; i++) {
+        if (!strcmp(argv[i], "--help") || !strcmp(argv[i], "-h")) {
+            print_help();
+            return EXIT_SUCCESS;
+        }
+
+        if (!strcmp(argv[i], "--clean") || !strcmp(argv[i], "-c")) {
+            return clean(files_to_remove, nx_len(files_to_remove));
+        }
+    }
 
     /* clang-format off */
     const char *glad_args[] = {
