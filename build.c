0f1f267ecf5f17194b69f9c0f75f47d405781747
xlc-dev <119954023+xlc-dev@users.noreply.github.com>
2024-12-21 21:54:44 +0100

    Compile glad and link to glfw and math

--- build.c
+++ build.c
@@ -15,14 +15,30 @@ int main(int argc, char **argv) {
     NX_REBUILD(argc, argv);
 
     /* clang-format off */
+    const char *glad_args[] = {
+        "cc",
+        "-c",
+        "glad/glad.c",
+        "-o",
+        "glad.o"
+    };
+
     const char *nexus_args[] = {
         "cc",
-        "nexus.c",
+        "main.c",
+        "glad.o",
         "-o",
         "nexus",
-        COMMON_FLAGS,
+        "-lglfw",
+        "-lm",
+        COMMON_FLAGS
     };
     /* clang-format on */
+
+    int glad_result = nx_compile_command("glad", glad_args, nx_len(glad_args), 0);
+    if (glad_result != 0) {
+        return glad_result;
+    }
 
     int nexus_result = nx_compile_command("nexus", nexus_args, nx_len(nexus_args), 1);
     if (nexus_result != 0) {
